1f96ec7a62160e327677c023d09abc924323b3ff
yozp <a1371361879@outlook.com>
2026-02-23 10:29:13 +0800

    feat(cart): 集成Nacos服务注册发现实现服务间调用
    - 在cart-service中添加Nacos服务发现依赖
    - 在item-service中添加Nacos服务发现依赖
    - 配置cart-service连接Nacos服务器地址
    - 配置item-service连接Nacos服务器地址
    - 在CartServiceImpl中注入DiscoveryClient用于服务发现
    - 使用RandomUtil实现负载均衡随机选择服务实例
    - 将硬编码的服务调用地址改为通过服务发现动态获取
    - 移除本地服务调用的固定端口依赖

--- cart-service/src/main/java/com/hmall/cart/service/impl/CartServiceImpl.java
+++ cart-service/src/main/java/com/hmall/cart/service/impl/CartServiceImpl.java
@@ -1,6 +1,7 @@
 package com.hmall.cart.service.impl;
 
 import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.util.RandomUtil;
 import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -15,6 +16,8 @@ import com.hmall.common.utils.BeanUtils;
 import com.hmall.common.utils.CollUtils;
 import com.hmall.common.utils.UserContext;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -45,6 +48,9 @@ public class CartServiceImpl extends ServiceImpl<CartMapper, Cart> implements IC
 
     @Resource
     private RestTemplate restTemplate;
+
+    @Resource
+    private DiscoveryClient discoveryClient;
 
     @Override
     public void addItem2Cart(CartFormDTO cartFormDTO) {
@@ -89,9 +95,11 @@ public class CartServiceImpl extends ServiceImpl<CartMapper, Cart> implements IC
         Set<Long> itemIds = vos.stream().map(CartVO::getItemId).collect(Collectors.toSet());
         // 2.查询商品
         // List<ItemDTO> items = itemService.queryItemByIds(itemIds);
+        List<ServiceInstance> instances = discoveryClient.getInstances("item-service");
+        ServiceInstance instance = instances.get(RandomUtil.randomInt(instances.size()));
         // 2.1.利用RestTemplate发起http请求，得到http的响应
         ResponseEntity<List<ItemDTO>> response = restTemplate.exchange(
-                "http://localhost:8081/items?ids={ids}",
+                instance.getUri() + "/items?ids={ids}",
                 HttpMethod.GET,
                 null,
                 new ParameterizedTypeReference<List<ItemDTO>>() {
